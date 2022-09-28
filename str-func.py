5b3e25a454ad92eac5692c47fb701592ba6578ca
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-09-28 09:45:38 -0300

    add func for str on python

---
+++ str-func.py
@@ -0,0 +1,16 @@
+teste = "teste"
+
+print(teste.upper())
+print(teste.lower())
+print(teste.title())
+
+hello = "  Olá mundo  "
+
+print(hello.strip() + "=")
+print(hello.rstrip() + ".")
+print(hello.lstrip() + "-")
+
+menu = " Python "
+
+print(menu.center(30,"#"))
+print("-".join(menu))
